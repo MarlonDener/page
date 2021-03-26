dd964b1336ce72371933c08750820250feace069
MarlonDener <m.dener01@hotmail.com>
2021-03-26 01:26:42 -0300

    Update portfólio

---
+++ slidesjs.js
@@ -0,0 +1,54 @@
+$(function(){
+	$('.mosaico .container-mosaico .mosaico-wraper').slick({
+		centerMode:true,
+		slidesToShow:3,
+		arrows:false,		
+		dots:true,
+
+  autoplay: true,
+  autoplaySpeed: 2000,
+
+		responsive:[
+
+		{
+			breakpoint:900,
+			settings:{
+  			centerMode:true,
+  			slidesToShow: 2,
+				dots:true,
+				infinite:true,
+  				autoplay: true,
+  				autoplaySpeed: 2000,
+
+			}
+
+		},
+		{
+			breakpoint:650,
+			settings:{
+				arrows:false,
+				centerMode:true,				
+				dots:true,
+				slidesToShow:1,				
+  				autoplay: true,
+  				autoplaySpeed: 2000,
+			}
+
+		},
+		{
+			breakpoint:380,
+			settings:{
+
+				dots:true,
+				arrows:false,
+				centerMode:true,
+				slidesToShow:1,
+  				autoplay: true,
+  				autoplaySpeed: 2000,
+			}
+
+		},
+
+		]
+	});
+})
