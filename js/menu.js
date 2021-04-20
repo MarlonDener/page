44f89bb24396a86f5b23dda942ccb055599dc689
Marlon Dener <70349830+MarlonDener@users.noreply.github.com>
2021-04-20 15:45:14 -0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
-function menuToggle(){
-	const menu = document.querySelector(".menu-icon");
-	menu.classList.toggle("active");
-	const sideBar = document.querySelector(".sideBar");
-	sideBar.classList.toggle("active");
 
+const menu = document.querySelector(".menu-icon");
+const sideBar = document.querySelector(".sideBar");
+
+function menuToggle(){
+	menu.classList.toggle("active");
+	sideBar.classList.toggle("active");
+}
+
+var close = document.querySelectorAll(".close");
+
+close.forEach(element => {
+	element.addEventListener("click", closeSideBar);
+});
+
+function closeSideBar(){
+	
+	if(sideBar.classList.contains("active")){
+		sideBar.classList.remove("active");
+		menu.classList.remove("active");
+	}
 }
